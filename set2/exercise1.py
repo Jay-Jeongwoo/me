48f233168e68c2a5efe88a59489dd5fee910ee1b
Jay Hong <z5305785@unsw.edu.au>
2021-06-13 22:12:01 +0900

    Result of running the tests.
    This first commit of week2, I will make better result from now on.

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -13,17 +13,21 @@ print("hello! Let's get started")  # it printed "hello! Let's get started"
 
 some_words = ['what', 'does', 'this', 'line', 'do', '?']
 
+# I think this will print "what", "does", "this", "line", "do", "?" in separate lines.
 for word in some_words:
     print(word)
+# I guess this will make a loop(iteration) since we use 'for'. 
+# Therefore, it will print all the componants from ['what' 'does' 'this' 'line' 'do' '?']
 
 for x in some_words:
     print(x)
+# This will print exact same result with the upper one.
 
 print(some_words)
 
 if len(some_words) > 3:
     print('some_words contains more than 3 words')
-
+# I think this will print 'some_words contatins more than 3 words' since it meet the requirement.
 def usefulFunction():
     """
     You may want to look up what uname does before you guess
@@ -31,5 +35,5 @@ def usefulFunction():
     https://docs.python.org/3/library/platform.html#platform.uname
     """
     print(platform.uname())
-
+# I think it will print user interface of my computer.
 usefulFunction()
--- set2/readme.md
+++ set2/readme.md
@@ -1,1 +1,16 @@
 TODO: Reflect on what you learned this week and what is still unclear.
+
+I thought that this week's exercises are pretty good to handle by myself until I encountered exercise 3.
+
+Before I begin to write about exercise 3, I would like to describe the moment that I almost completed exercise 0, 1, and 2 and then my laptop got frozen.
+
+No one knows why it occured. It just happened.
+
+However, this incident taught me the importance of saving your working file as many times as possible. 
+
+Anyway, about exercise 0, making the uppercase was a sort of challenge at first.
+
+I googled it first and then applied a code. (I don't know why but I didn't tried many tests at the moment.)
+
+Concerning exercise1, it could be most easiest homework since all I have to do is just making comments on the codes.
+
