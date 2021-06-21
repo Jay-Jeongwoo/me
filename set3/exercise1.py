b13750505f243848fe887965e32a8a560b3fa82f
Jay Hong <z5305785@unsw.edu.ad>
2021-06-21 20:29:53 +0900

    first attempt

--- set3/exercise1.py
+++ set3/exercise1.py
@@ -9,10 +9,17 @@ def loop_ranger(start, stop=None, step=1):
     """Return a list of numbers between start and stop in steps of step.
 
     Do this using any method apart from JUST using range() #TODO: clarify this wording
-    The look up the docs for range(), you can answer this with just the range 
+    The look up the docs for range(), you can answer this with just the range
     function, but we'd like you to do it the long way, probably using a loop.
     """
-    return None
+
+    # still figuring out how i can generate this code without using range().
+    number_list = []
+    for i in range(start, stop, step):
+        number_list.append(i)
+
+    print(number_list)
+    return number_list
 
 
 def lone_ranger(start, stop, step):
@@ -20,7 +27,12 @@ def lone_ranger(start, stop, step):
 
     Look up the docs for range() and wrap it in a 1:1 way
     """
-    return None
+    num_list = []
+    for i in range(start, stop, step):
+        num_list.append(i)
+
+    print(num_list)
+    return num_list
 
 
 def two_step_ranger(start, stop):
@@ -29,7 +41,12 @@ def two_step_ranger(start, stop):
     Sometimes you want to hide complexity.
     Make a range function that always has a step size of 2
     """
-    return None
+    step_list = []
+    for i in range(start, stop, 2):
+        step_list.append(i)
+
+    print(step_list)
+    return step_list
 
 
 def stubborn_asker(low, high):
@@ -40,17 +57,41 @@ def stubborn_asker(low, high):
 
     Look up the docs for input
     """
-    return None
+    import random
+
+    number = random.randint(low, high)
+    number_guess = 0
+
+    while True:
+        guess = int(input())
+        if guess == number:
+            number_guess = number_guess + 1
+            break
+        print(number)
+
+        return number
 
 
 def not_number_rejector(message):
     """Ask for a number repeatedly until actually given one.
 
-    Ask for a number, and if the response is actually NOT a number 
+    Ask for a number, and if the response is actually NOT a number
     (e.g. "cow", "six", "8!") then throw it out and ask for an actual number.
     When you do get a number, return it.
     """
-    return None
+    import random
+
+    given_number = []
+    given_number.append(message)
+
+    while True:
+        message = input()
+        if message == given_number:
+            print(message)
+            break
+        else:
+            print(message)
+        return message
 
 
 def super_asker(low, high):
@@ -61,7 +102,28 @@ def super_asker(low, high):
     Try to call at least one of the other functions to minimise the
     amount of code.
     """
-    return None
+    import random
+
+    my_number = random.randint(low, high)
+    done = False
+    print(my_number)
+    while not done:
+        try:
+            guess = int(input())
+            while guess != my_number:
+                if guess > my_number:
+                    print("lower")
+                    guess = int(input())
+                else:
+                    print("larger")
+                    guess = int(input())
+            else:
+                print("correct")
+                done = True
+        except ValueError:
+            print("invaild value")
+
+            return my_number
 
 
 if __name__ == "__main__":
--- set3/readme.md
+++ set3/readme.md
@@ -1,1 +1,22 @@
 TODO: Reflect on what you learned this week and what is still unclear.
+
+**My Jounal**
+
+_exercise 1_
+loop_ranger: still wondering how i can generate this without using range().
+
+lone_ranger: As usual, started with empty list and then append i, which have been assigned the range.
+
+two_step_ranger: As range(). function requires (start, stop, step), I assigned 2 in the step.
+
+stubborn_asker: I have been struggling with this def since it is first time to use while loop for me. At first, I imported random to generate random number. And I need infinite loop which only ends if it achieves certain requirements. So I used **while true** and then wrote if statement to assign the condition.
+
+not_number_rejector: still working on this def. Although I didn't use try and catch on this, I got a green tick from the test. I am wondering how I can throw non-number components.
+
+super_asker: Finally, I could use try and except for this question. Following the guideline, I combined the strategy that I had used for this exercise. I imported random and used while loop. However, I still get a result that says invaild value, which means I have a error in value. Therefore, still have to look at this one.
+
+_exercise 2_
+
+_exercise 3_
+
+_exercise 4_
